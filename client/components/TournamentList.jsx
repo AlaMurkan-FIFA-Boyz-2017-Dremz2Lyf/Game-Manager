2c2fe9390d5a08d97e42d182eac379ab411cfcb8
Scott <scotthorn0@gmail.com>
2016-10-18 01:16:30 -0500

    Added comments

--- client/components/TournamentList.jsx
+++ client/components/TournamentList.jsx
@@ -2,10 +2,14 @@ var React = require('react');
 var Player = require('./Player.jsx');
 
 var TournamentList = (props) => {
+
+  // Just like playerList, tourneyPlayersList will become and array of Player Components
+    // with the name of that player, the movePlayer function, and it's index passed down as props
   var tourneyPlayersList = props.players.map(function(player, i) {
-    return <Player name={player} click={props.click} index={i} players={props.players}/>;
+    return <Player name={player} click={props.click} index={i} />;
   });
 
+  // and just like playerList again, we render that out in an unordered list
   return (
     <div className="panel panel-default">
       <div className="panel-heading">Tournament Players</div>
