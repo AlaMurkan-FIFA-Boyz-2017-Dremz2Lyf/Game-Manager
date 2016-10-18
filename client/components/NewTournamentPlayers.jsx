4c0a982284faf4ae8c33686620a0264069c7ae7d
Scott <scotthorn0@gmail.com>
2016-10-18 11:24:59 -0500

    Updated movePlayer function to work with API data

--- client/components/NewTournamentPlayers.jsx
+++ client/components/NewTournamentPlayers.jsx
@@ -1,25 +1,25 @@
 var React = require('react');
 var Player = require('./Player.jsx');
 
-var TournamentList = (props) => {
+var NewTournamentPlayers = (props) => {
 
   // Just like playerList, tourneyPlayersList will become and array of Player Components
     // with the name of that player, the movePlayer function, and it's index passed down as props
-  var tourneyPlayersList = props.players.map(function(player, i) {
-    return <Player name={player} click={props.click} index={i} />;
+  var newTourneyPlayersList = props.players.map(function(player, i) {
+    return <Player playerObj={player} click={props.click} index={i} />;
   });
 
   // and just like playerList again, we render that out in an unordered list
   return (
     <div className="panel panel-default">
-      <div className="panel-heading">Tournament Players</div>
+      <div className="panel-heading">New Tournament Players</div>
       <div className="panel-body">
         <ul className="list-group">
-          {tourneyPlayersList}
+          {newTourneyPlayersList}
         </ul>
       </div>
     </div>
   );
 };
 
-module.exports = TournamentList;
+module.exports = NewTournamentPlayers;
