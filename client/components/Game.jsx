245ca17963d1c87c368a0e5f70f05f713deffea5
benjimelito <melito.ben@gmail.com>
2016-10-18 14:58:44 -0500

    Getting Game component in line with bootstrap styling

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -1,7 +1,19 @@
 var React = require('react');
 
+// var Game = (props) => (
+//   <li className="list-group-item">{props.player1_name} : {props.player1_score} {props.player2_name} : {props.player2_score}</li>  
+// );
+
 var Game = (props) => (
-  <li className="list-group-item">{props.player1_name} : {props.gameObj.player1_score} {props.player2_name} : {props.gameObj.player2_score}</li>  
-); //Need to add in a click, also hold off on the way we're getting names for now
+	<li className="list-group-item">
+	  <span className="player1_name">{props.player1_name}</span>
+	  <span className="score">
+	    <span classNAme="player1_score">{props.gameObj.player1_score}</span>
+	     -
+	    <span className="player2_score">{props.gameObj.player2_score}</span>
+	  </span>
+	  <span className="player2_name">{props.player2_name}</span>
+    </li>
+);
 
 module.exports = Game;
