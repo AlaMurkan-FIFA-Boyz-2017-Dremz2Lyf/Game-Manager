0149979a2997b89c72d0695dfd4551ead8528d82
Scott <scotthorn0@gmail.com>
2016-10-18 00:16:51 -0500

    Steps towards lists rendering with bootstrap, className! :D

--- client/components/TournamentList.jsx
+++ client/components/TournamentList.jsx
@@ -2,11 +2,21 @@ var React = require('react');
 var Player = require('./Player.jsx');
 
 var TournamentList = (props) => (
-  <ul>
-    {props.players.map(function(player, i) {
-      return <Player name={player} click={props.click} index={i} players={props.players}/>;
-    })}
-  </ul>
+  var tourneyPlayersList = props.players.map(function(player, i) {
+    return
+    <li className="list-group-item">
+      <Player name={player} click={props.click} index={i} players={props.players}/>;
+    </li>
+  })
+  <div className="panel panel-default">
+    <div className="panel-heading">Tournament Players</div>
+    <div className="panel-body">
+      <ul className="list-group">
+        {tourneyPlayersList}
+      </ul>
+    </div>
+  </div>
+
 );
 
 module.exports = TournamentList;
