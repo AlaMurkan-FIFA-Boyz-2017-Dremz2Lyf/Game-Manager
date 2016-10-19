cd5a4465814ccdc237656bd71380e4aba10661f4
Nick Wise <nwise225@gmail.com>
2016-10-19 18:28:55 -0500

    Turned autocomplete off on GameStatsForm and added some styling. Cleaned up code

--- client/components/GameStatsForm.jsx
+++ client/components/GameStatsForm.jsx
@@ -1,26 +1,6 @@
 var React = require('react');
 var axios = require('axios');
 
-//When the PUT has happened need to run a function that updates the state of current game score in 
-//the Main currentGame state
-
-//Need to show an error when a game hasn't been selected to update scores for. 
-
-//Bugs
-//If no game is clicked there won't be a current game and if you try to input scores it send you back to home. Not sure where this error need to be handled
-//Error handling so it doesn't send the user back to the home page if they forget to click on the game they want to update with the scores
-//Maybe want to auto set the first game to current so that there won't be an issue of the user forgetting to click the game to update and sending back to home
-
-
-//Testing
-//Multple games
-//If user clicks on page outside of the games listed will it fuck up or will it remember which game the user clicked on last -->Remembers which game was clicked last
-
-//Need to show the user which game is current with some styling so it is obvious. As of right now there is no feedback
-//Fix styling for my form
-//But I still need to handle the error in case we don't implement that
-
-//Need an error if the user forgets to submit a score for one of the player otherwise it just shows nothing on that players side
 
 class GameStatsForm extends React.Component {
 
@@ -48,8 +28,7 @@ class GameStatsForm extends React.Component {
       
   insertStats(event) {
     var self = this;
-    var tourneyId = this.props.currentGame.tournament_id
-    console.log('GAMESTATS TOURNEYID', this.props.currentGame.tournament_id)
+    var tourneyId = this.props.currentGame.tournament_id;
     event.preventDefault();
     axios.put('/api/games', {
       id: self.props.currentGame.id,
@@ -57,39 +36,39 @@ class GameStatsForm extends React.Component {
       player2_score: this.state.player_2_score
     })
     .then(function() {
-      self.props.updateGameStats(tourneyId)
+      self.props.updateGameStats(tourneyId);
       self.setState({
         player_1_score: '',
         player_2_score: ''
       })
     })
     .catch(function(err) {
-      res.status(500).send('Error Inserting Player Scores')
+      res.status(500).send('Error Inserting Player Scores');
     })
 }
+            // <label htmlFor="player1_id">Home</label>
+            // <label htmlFor="player2_id" >Away</label>
 
   render() {
     return (
 
-    <form className="form-inline" onSubmit={this.insertStats.bind(this)} >
-          <div className="form-group">
-            <label htmlFor="player1_id">Home</label>
+    <form className="form-inline" onSubmit={this.insertStats.bind(this)} autoComplete="off">
+          <div className="form-group form-group-sm">
             <input type="text"
-              className="form-control player1-score"
+              className="form-control player1-score col-xs-2"
               id="player1_id"
               value={this.state.player_1_score}
               onChange={this.handleInputChangeHome.bind(this)} 
               placeholder="Home Final Score" />
             
-            <label htmlFor="player2_id" >Away</label>
             <input type="text"
-              className="form-control player2-score" 
+              className="form-control player2-score col-xs-2" 
               id="player2_id"
               value={this.state.player_2_score}
               onChange={this.handleInputChangeAway.bind(this)} 
               placeholder="Away Final Score" /> 
           </div>
-          <button type="submit" className="btn btn-default">Submit</button>
+          <button type="submit" className="btn btn-default btn-xs">Submit</button>
         </form>
     );    
   }
--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -175,9 +175,8 @@ class Main extends React.Component {
     });
   }
 
-//THIS IS WHERE I AM MAKING CHANGES TO MAKE MY GAMESTATSFORM UPDATE THE GAME SCORES AFTER SUBMIT
+//GameStatsForm calls this function after it has PUT the entered stats in the database.
   updateGameStats(tourneyId) {
-    console.log('This should be tourney ID', tourneyId)
     //reusing the api call from createGames to make a call to the database with the updated game stats
     var self = this;
     axios.get('/api/games', {
@@ -185,8 +184,9 @@ class Main extends React.Component {
           tournament_id: tourneyId
         }
       }).then(function(response) {
-
-        // then if the games post was Successful, we set inProgress to true
+        //Here it will show the updated game scores for each game that scores have been entered in. The GameStatsForm
+        //PUTs the scores to the database then here we GET from the database to gather the new scores and show them on 
+        //the page
         var games = response.data;
         self.setState({
           currentTournamentGames: games,
