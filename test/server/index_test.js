f4755992972671906528bcf8cd9681ff3c86576a
Scott <scotthorn0@gmail.com>
2016-10-20 00:12:02 -0500

    Server tests broken out into routes

--- test/server/index_test.js
+++ test/server/index_test.js
@@ -8,18 +8,39 @@ describe('The Server', function() {
   var app = TestHelper.createApp();
   app.use('/', routes);
   app.testReady();
+  // Notice how these are generator functions (indicated by the the *)
+  // See test/test-helper.js for details of why this works.
+  // I (Scott) have no clue, but it seems to be working right now.......
+  describe('route /api/games', function() {
+    it_('"get /api/games" should be a route in the server', function * () {
 
-  it_('Returns an Array when "get /api/games" is queried with a tournament_id', function * () {
+      yield request(app)
+      .get('/api/games')
+      .expect(200);
+    });
 
-    //
-    // Notice how we're in a generator function (indicated by the the *)
-    // See test/test-helper.js for details of why this works.
-    //
-    yield request(app)
+    it_('Returns an Array when "get /api/games" is queried with a tournament_id', function * () {
+
+      yield request(app)
       .get('/api/games?tournament_id=3')
       .expect(200)
       .expect(function(response) {
         expect(response.body).to.be.a('array');
       });
+    });
+
   });
+
+  describe('route /api/games/stats', function() {
+
+    it_('"get /api/games/stats" should be a route in the server', function * () {
+
+      yield request(app)
+      .get('/api/games/stats')
+      .expect(200);
+    });
+
+  });
+
+
 });
