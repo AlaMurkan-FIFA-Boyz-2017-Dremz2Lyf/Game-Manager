53d03b057e1d2a9755928943a8352f688b3e53c0
Scott <scotthorn0@gmail.com>
2016-10-20 02:17:02 -0500

    More tests written, on the way.

--- test/server/index_test.js
+++ test/server/index_test.js
@@ -1,5 +1,6 @@
 require(TEST_HELPER); // <--- This must be at the top of every test file.
 
+var helpers = require(__server + '/serverHelpers.js');
 var request = require('supertest-as-promised');
 var routes = require(__server + '/index.js');
 
@@ -11,15 +12,55 @@ describe('The Server', function() {
   // Notice how these are generator functions (indicated by the the *)
   // See test/test-helper.js for details of why this works.
   // I (Scott) have no clue, but it seems to be working right now.......
-  describe('route /api/games', function() {
-    it_('"get /api/games" should be a route in the server', function * () {
 
+  describe('Helper functions', function() {
+
+    it_('Helper functions should be available', function * () {
+      expect(helpers).to.exist;
+    });
+
+    describe('createGamesForTourney', function() {
+
+      it_('helper should have a "createGamesForTourney" function', function * () {
+        expect(helpers.createGamesForTourney).to.be.an('function');
+      });
+
+      it_('When called with a tournament id and array of player objects it should return an array of game objects', function * () {
+        var tournamentId = 1;
+        var playersList = Mock_DataBase.players;
+
+        var result = helpers.createGamesForTourney(tournamentId, playersList);
+
+        expect(result).to.be.an('array');
+        expect(result[0]).to.be.an('object');
+        expect(result[0]).to.have.all.keys('player1_id', 'player2_id', 'tournament_id');
+      });
+
+    });
+    describe('getPlayer', function() {
+
+      it_('helper should have a getPlayer function', function * () {
+        expect(helpers.getPlayer).to.be.a('function');
+      });
+
+      it_('When called with a player id it should return the player object from the database', function * () {
+        expect(helpers.getPlayer());
+      });
+    });
+
+
+  });
+
+  describe('route /api/games', function() {
+
+    it_('"get /api/games" should be a route in the server', function * () {
+      console.log('NOTE: The get all games route will be made when we want to make a player stats page');
       yield request(app)
       .get('/api/games')
       .expect(200);
     });
 
-    it_('Returns an Array when "get /api/games" is queried with a tournament_id', function * () {
+    it_('Should return an Array when "get /api/games" is queried with a tournament_id', function * () {
 
       yield request(app)
       .get('/api/games?tournament_id=3')
@@ -39,6 +80,37 @@ describe('The Server', function() {
       .get('/api/games/stats')
       .expect(200);
     });
+
+    it_('Should return an Array when "get /api/games/stats" is queried with a tournament_id', function * () {
+
+      yield request(app)
+      .get('/api/games/stats')
+      .expect(200)
+      .expect(function(response) {
+        expect(response.body).to.be.a('array');
+      });
+    });
+
+    it_('the response body should be an array of objects', function * () {
+
+      yield request(app)
+      .get('/api/games/stats')
+      .expect(200)
+      .expect(function(response) {
+        expect(response.body[0]).to.be.a('object');
+      });
+    });
+
+    it_('the objects in the return array should be properly formatted', function * () {
+
+      yield request(app)
+      .get('/api/games/stats')
+      .expect(200)
+      .expect(function(response) {
+        expect(response.body[0]).to.have.all.keys('name', 'gp', 'won', 'loss', 'draw', 'gd', 'points');
+      });
+    });
+
 
   });
 
--- test/test-helper.js
+++ test/test-helper.js
@@ -1,57 +1,82 @@
-process.env.NODE_ENV = 'test'
+process.env.NODE_ENV = 'test';
 
 // The following allows you to require files independent of
 // the location of your test file.
 // Example:
 //  var User = require(__server + '/models/user.js')
 //
-global.__server = __dirname + '/../server'
-global.__client = __dirname + '/../client'
+global.__server = __dirname + '/../server';
+global.__client = __dirname + '/../client';
 
 //
 // Assertions
 //
-var chai = require('chai')
+var chai = require('chai');
 // Option 1: Make the `expect` function available in every test file
-global.expect = chai.expect
+global.expect = chai.expect;
 // Option 2: Make everything should-able
 // global.should = chai.should()
 
+
+// This is the object to create the mock database.
+  // It should contain keys representing tables in the db.
+  // Those keys should point to arrays holding objects or 'rows' back from the DB
+global.Mock_DataBase = {};
+
+// set a players key on the mock database
+Mock_DataBase.players = [
+  {id: 1, username: 'Alice'},
+  {id: 2, username: 'Bob'},
+  {id: 3, username: 'Gilbert'}
+];
+
+// set a tournaments key on the mock database
+Mock_DataBase.tournaments = [
+  {id: 1, tournament_name: 'Super Tourney!', winner_id: null}
+];
+// set a games key on the mock database
+Mock_DataBase.games = [
+  {id: 1, player1_id: 1, player2_id: 2, player1_score: null, player2_score: null, player1_shots: null, player2_shots: null, player1_possession: null, player2_possession: null, player1_shotsOnGoal: null, player2_shotsOnGoal: null, tournament_id: 1},
+  {id: 2, player1_id: 1, player2_id: 3, player1_score: null, player2_score: null, player1_shots: null, player2_shots: null, player1_possession: null, player2_possession: null, player1_shotsOnGoal: null, player2_shotsOnGoal: null, tournament_id: 1},
+  {id: 3, player1_id: 2, player2_id: 3, player1_score: null, player2_score: null, player1_shots: null, player2_shots: null, player1_possession: null, player2_possession: null, player1_shotsOnGoal: null, player2_shotsOnGoal: null, tournament_id: 1}
+];
 
 //
 // Helper Functions
 //
 // This is the object you can attach any helper functions used across
 // several test files.
-global.TestHelper = {}
+global.TestHelper = {};
 
+//
 //
 // Mock apps for API testing
 //
-var express = require('express')
+var express = require('express');
 
 TestHelper.createApp = function (loader) {
-  var app = express()
-  app.use(require('body-parser').json())
+  var app = express();
+  app.use(require('body-parser').json());
 
   app.testReady = function () {
     // Log all errors
     app.use(function (err, req, res, next) {
-      console.error("==Error==")
-      console.error("   " + err.stack)
-      next(err)
-    })
-  }
-  return app
-}
+      console.error('==Error==');
+      console.error('   ' + err.stack);
+      next(err);
+    });
+  };
+  return app;
+};
+
 
 //
 // Mocha "helpers" to support coroutines tests
 //
-var Bluebird = require('bluebird')
+var Bluebird = require('bluebird');
 
-global.before_ = function (f) { before ( Bluebird.coroutine(f) ) }
-global.beforeEach_ = function (f) { beforeEach ( Bluebird.coroutine(f) ) }
-global.it_ = function (description, f) { it ( description, Bluebird.coroutine(f) ) }
-global.xit_ = function (description, f) { xit ( description, f ) }
-global.it_.only = function (description, f) { it.only( description, Bluebird.coroutine(f) ) }
+global.before_ = function (f) { before ( Bluebird.coroutine(f) ); };
+global.beforeEach_ = function (f) { beforeEach ( Bluebird.coroutine(f) ); };
+global.it_ = function (description, f) { it ( description, Bluebird.coroutine(f) ); };
+global.xit_ = function (description, f) { xit ( description, f ); };
+global.it_.only = function (description, f) { it.only( description, Bluebird.coroutine(f) ); };
