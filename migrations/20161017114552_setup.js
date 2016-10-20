0c00a2c551c30f4c02b8a4cc84583a7f4b9f1c86
Nick Wise <nwise225@gmail.com>
2016-10-20 14:45:41 -0500

    Added NotNull to username and tournament name. Added status column to games. Migrated to DB so also added the DB to this commit with new values

--- migrations/20161017114552_setup.js
+++ migrations/20161017114552_setup.js
@@ -3,11 +3,11 @@ exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('players', function(table) {
       table.increments('id').primary();
-      table.string('username').unique();
+      table.string('username').unique().notNullable();
     }),
     knex.schema.createTable('tournaments', function(table) {
       table.increments('id').primary();
-      table.string('tournament_name').unique();
+      table.string('tournament_name').unique().notNullable();
       table.integer('winner_id').references('id').inTable('players');
     }),
     knex.schema.createTable('games', function(table) {
@@ -23,6 +23,7 @@ exports.up = function(knex, Promise) {
       table.integer('player1_shotsOnGoal').unsigned();
       table.integer('player2_shotsOnGoal').unsigned();
       table.integer('tournament_id').references('id').inTable('tournaments');
+      table.string('status');
 
     })
   ]);
