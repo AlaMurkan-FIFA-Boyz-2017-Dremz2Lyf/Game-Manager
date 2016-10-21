13f7d7ad6fd505f1bb72a33c08ece4005cc57477
Scott <scotthorn0@gmail.com>
2016-10-21 15:00:37 -0500

    Status class added on game list elements

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -5,14 +5,14 @@ var React = require('react');
 // );
 
 var Game = (props) => (
-	<li className="list-group-item "/* + {props.status}*/ onClick={function() { props.click(props.gameIndex); }}>
-	  <span className="player1">{props.player1_name}</span>
-	  <span className="score">
-	    <span className="player1_score">{props.gameObj.player1_score}</span>
-	     -
-	    <span className="player2_score">{props.gameObj.player2_score}</span>
-	  </span>
-	  <span className="player2">{props.player2_name}</span>
+    <li className={'list-group-item ' + props.status} onClick={function() { props.click(props.gameIndex); }}>
+      <span className="player1">{props.player1_name}</span>
+      <span className="score">
+        <span className="player1_score">{props.gameObj.player1_score}</span>
+          -
+        <span className="player2_score">{props.gameObj.player2_score}</span>
+      </span>
+      <span className="player2">{props.player2_name}</span>
     </li>
 );
 
