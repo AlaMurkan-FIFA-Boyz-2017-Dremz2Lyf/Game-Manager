a03dc635105f8073c8f5fa40a19378659baa7e08
benjimelito <melito.ben@gmail.com>
2016-10-23 20:36:34 -0500

    Added production settings

--- knexfile.js
+++ knexfile.js
@@ -28,20 +28,11 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database.sqlite3'
+    },
+  }
 
 };
