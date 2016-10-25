37aaecf032e408d9e81518a585fd4e553aa15589
Calvin Kniffin <calkniffin@gmail.com>
2016-10-25 17:02:15 -0500

    Adds react native buttons for google/fb login (#9)

---
+++ client/components/Fb.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var fb = require('../fbAuth');
+var Button = require('react-native-button')
+
+var FbLogin = (props) => {
+  render() {
+    return (
+      <Button
+        style={{ borderWidth: 1, borderColor='blue'}}
+        onPress={ fb.fbSignIn }>
+          Sign in with Facebook
+        </Button>
+    )
+  }
+}
+
+var FbLogout = (props) => {
+  render() {
+    return (
+      <Button
+        style={{ borderWidth: 1, borderColor: 'blue'}}
+        onPress={ fb.fbSignOut }>
+          Sign out
+        </Button>
+    )
+  }
+}
---
+++ client/components/google.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var google = require('../googleAuth');
+var Button = require('react-native-button');
+
+var GoogleLogin = (props) => {
+  render() {
+    return (
+      <Button
+        style={{ borderWidth: 1, borderColor='green'}}
+        onPress={ google.googleSignIn }>
+          Sign in with Google
+        </Button>
+    )
+  }
+}
+
+var GoogleLogout = (props) => {
+  render() {
+    return (
+      <Button
+        style={{ borderWidth: 1, borderColor: 'green'}}
+        onPress={ google.googleSignOut }>
+          Sign out
+        </Button>
+    )
+  }
+}
