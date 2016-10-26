b41caaab829e8b73d9f606f868923ab3ee0098b8
Brett Nelson <brettcnelson@gmail.com>
2016-10-26 16:53:15 -0500

    draft database calls (#20)

--- client/components/StatsTable.jsx
+++ client/components/StatsTable.jsx
@@ -1,11 +1,25 @@
 var StatLine = require('./StatLine.jsx');
 var React = require('react');
+var database = firebase.database();
 
 var StatsTable = (props) => {
+
+//   getStats(sortBy) {
+//     return firebase.database().ref('/users').once('value')
+//       .then(snapshot) {
+
+//       }
+//   }
+  
+//   return firebase.database().ref('/users/' + userId).once('value')
+//     .then(function(snapshot) {
+//       var username = snapshot.val().username;
+// });
 
   // var statLines = props.table.map(function(playerStats, index) {
   //   return <StatLine playerStats={playerStats} key={index} />;
   // });
+
   var testArray = [{name: 'calvin', gp: 3, won: 3, draw: 0, loss: 8, gd: -3, points: 8}, {name: 'brett', gp: 7, won: 4, draw: 8, loss: 3, gd: 7, points: 6}]
   var statLines = testArray.map(function(x) {
     return <StatLine playerStats={x}/>
