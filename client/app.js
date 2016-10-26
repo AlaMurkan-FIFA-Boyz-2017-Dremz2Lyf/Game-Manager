71fe1362026d831c7ebd1763a426a38fbbd5a1ef
Zach <zachraymer@gmail.com>
2016-10-26 12:42:19 -0500

    changed file paths and make new directories

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Main = require('./components/Main.jsx');
+var Main = require('./components/fifa/Main.jsx');
 
 ReactDOM.render(
   <Main />, // JSX
