2d7767bc23f62f62930352519501955d42c81043
Zachary Raymer <zachraymer@gmail.com>
2016-10-26 15:20:32 -0500

    fixes the broken (#17)

---
+++ client/components/StartTournament.jsx
@@ -0,0 +1,64 @@
+var React = require('react');
+var axios = require('axios');
+
+class StartTournament extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      tourneyName: '',
+      error: null
+    };
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      tourneyName: event.target.value
+    });
+  }
+
+  startTourney(event) {
+    var self = this;
+    event.preventDefault();
+    this.props.createTournament(this.state.tourneyName)
+    .then(function(res) {
+      self.setState({
+        error: null
+      });
+    })
+    .catch(function(err) {
+      self.setState({
+        error: err.response.data.message
+      });
+    });
+
+  }
+
+  render() {
+
+
+    var buttonClass = this.state.error ? 'btn btn-danger tourney-exists-btn' : 'btn btn-default';
+    var buttonContent = this.state.error ? 'Whoops' : 'Create';
+
+    return (
+        <form className="form-inline" onSubmit={this.startTourney.bind(this)} autoComplete="off">
+          <div className="form-group">
+            <input type="text"
+              className="form-control"
+              id="tourneyName"
+              placeholder="Tournament Name"
+              onChange={this.handleInputChange.bind(this)} />
+          </div>
+          <button type="submit" className={buttonClass}>{buttonContent.toUpperCase()}</button>
+          <p className="tourney-oops">{this.state.error}</p>
+        </form>
+      );
+    // }
+
+  }
+}
+
+
+
+module.exports = StartTournament;
