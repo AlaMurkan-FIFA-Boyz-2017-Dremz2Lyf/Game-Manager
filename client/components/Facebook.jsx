ecc6d820e892a94fedc4bb9c4ae8672e1807a37e
Brett Nelson <brettcnelson@gmail.com>
2016-10-26 19:38:43 -0500

    comment out auth files, fix statstable index error

--- client/components/Facebook.jsx
+++ client/components/Facebook.jsx
@@ -1,27 +1,27 @@
-var React = require('react');
-var fb = require('../fbAuth.js');
-var Button = require('react-native-button')
+// var React = require('react');
+// var fb = require('../fbAuth.js');
+// var Button = require('react-native-button')
 
-var FbLogin = (props) => {
-  render() {
-    return (
-      <Button
-        style={{ borderWidth: 1, borderColor='blue'}}
-        onPress={ fb.fbSignIn() }>
-          Sign in with Facebook
-        </Button>
-    )
-  }
-}
+// var FbLogin = (props) => {
+//   render() {
+//     return (
+//       <Button
+//         style={{ borderWidth: 1, borderColor='blue'}}
+//         onPress={ fb.fbSignIn() }>
+//           Sign in with Facebook
+//         </Button>
+//     )
+//   }
+// }
 
-var Logout = (props) => {
-  render() {
-    return (
-      <Button
-        style={{ borderWidth: 1, borderColor: 'blue'}}
-        onPress={ fb.signOut() }>
-          Sign out
-        </Button>
-    )
-  }
-}
+// var Logout = (props) => {
+//   render() {
+//     return (
+//       <Button
+//         style={{ borderWidth: 1, borderColor: 'blue'}}
+//         onPress={ fb.signOut() }>
+//           Sign out
+//         </Button>
+//     )
+//   }
+// }
