7830eb84c5c42374b71de0402bde39bab73cadaa
Brett Nelson <brettcnelson@gmail.com>
2016-10-28 10:58:06 -0500

    statsTable and tourneyName working again (#48)

--- client/components/StatsTable.jsx
+++ client/components/StatsTable.jsx
@@ -5,23 +5,10 @@ var React = require('react');
 
 var StatsTable = (props) => {
 
-//   getStats(sortBy) {
-//     return firebase.database().ref('/users').once('value')
-//       .then(snapshot) {
+  var statLines = props.table.map(function(user, i) {
+    return <StatLine playerStats={user} key={i} />;
+  });
 
-//       }
-//   }
-
-//   return firebase.database().ref('/users/' + userId).once('value')
-//     .then(function(snapshot) {
-//       var username = snapshot.val().username;
-// });
-
-  // var statLines = props.table.map(function(playerStats, index) {
-  //   return <StatLine playerStats={playerStats} key={index} />;
-  // });
-
-// need to make table column headers sort on click
   return (
   <div className="panel panel-default">
     <div className="panel-body">
@@ -29,11 +16,11 @@ var StatsTable = (props) => {
         <thead>
           <tr>
             <th>Player</th>
-            <th>GP</th>
-            <th>Won</th>
-            <th>Draw</th>
-            <th>Loss</th>
-            <th>GD</th>
+            <th>Games<br/>Played</th>
+            <th>Wins</th>
+            <th>Draws</th>
+            <th>Losses</th>
+            <th>Goal<br/>Differential</th>
             <th>Points</th>
           </tr>
         </thead>
--- client/components/Tournament.jsx
+++ client/components/Tournament.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 
 var Tournament = (props) => (
-  <li className="list-group-item" onClick={function() { props.click(props.index, props.tourneyObj.tourneyId); }}>{props.tourneyObj.tournament_name}</li>
+  <li className="list-group-item" onClick={function() { props.click(props.index, props.tourneyObj.tourneyId); }}>{props.tourneyObj.data.tourneyName}</li>
 );
 
 module.exports = Tournament;
